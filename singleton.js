ab944c244a01973c6e5eb1947c1cab6a92203c77
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-02-23 19:14:45 +0800

    单例模式

---
+++ singleton.js
@@ -0,0 +1,48 @@
+// 单例模式
+// 无论多少次初始化 或者使用某个函数 始终只有一个实例
+// 一般处理
+function createMask() {
+    if($(".mask")[0],length > 0){
+        // 如果有mask 不作处理
+        return ;
+    } 
+
+    $("body").appenChild($("<div class='mask'></div>"));
+
+}
+
+createMask();
+// 这样优点是思路简单 但是缺点也很明显 每次调用的时候 都要检查dom
+
+var mask;
+
+function createMask1() {
+    if (mask) return;
+
+    mask = $("<div class='mask'></div>");
+
+    return mask;
+}
+
+createMask1();
+
+// 这样使用一个全局变量 来表示是否应该显示mask
+// 这样基本可以是满足需求 但是有些人认为全局变量污染 希望不要使用全局变量 有了以下方法
+
+var singleton = function( fn ){
+    var result;
+    return function(){
+        return result || ( result = fn.apply( this, arguments ) );
+    }
+}
+ 
+var createMask3 = singleton(function(){
+ 
+    return document.body.appendChild($("<div class='mask'></div>"));
+ 
+})
+
+createMask3();
+
+// 这次使用了闭包 之前的全局变量变成了函数内部变量 避免了全局污染
+// 可以说上面这么做的模式是单例模式标准的形式 但是过于复杂
